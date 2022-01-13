231711d6cf9f710a442607cd1c494930754f42ba
navaneethrkrishna <95025235+navaneethrkrishna@users.noreply.github.com>
2022-01-13 16:33:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
+- This is a open source project. 
 - Source - https://github.com/navaneethrkrishna/Eva-TG
 
 <b>DEVS:</b>
